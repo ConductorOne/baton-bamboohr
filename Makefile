0bea7df54f6880bcbe50fc8678f3b6a0ff29e9ea
Javier David Carnelli <javier.carnelli@conductorone.com>
2025-07-21 12:01:24 -0300

    feat: coconnector updated for it to be containerized

--- Makefile
+++ Makefile
@@ -1,13 +1,30 @@
 GOOS = $(shell go env GOOS)
 GOARCH = $(shell go env GOARCH)
 BUILD_DIR = dist/${GOOS}_${GOARCH}
+GENERATED_CONF := pkg/config/conf.gen.go
+
+ifeq ($(GOOS),windows)
+OUTPUT_PATH = ${BUILD_DIR}/baton-bamboohr.exe
+else
 OUTPUT_PATH = ${BUILD_DIR}/baton-bamboohr
+endif
+
+# Set the build tag conditionally based on ENABLE_LAMBDA
+ifdef BATON_LAMBDA_SUPPORT
+	BUILD_TAGS=-tags baton_lambda_support
+else
+	BUILD_TAGS=
+endif
 
 .PHONY: build
-build:
-	rm -f ${OUTPUT_PATH}
-	mkdir -p ${BUILD_DIR}
-	go build -o ${OUTPUT_PATH} cmd/baton-bamboohr/*.go
+build: $(GENERATED_CONF)
+	go build ${BUILD_TAGS} -o ${OUTPUT_PATH} ./cmd/baton-bamboohr
+
+$(GENERATED_CONF): pkg/config/config.go go.mod
+	@echo "Generating $(GENERATED_CONF)..."
+	go generate ./pkg/config
+
+generate: $(GENERATED_CONF)
 
 .PHONY: update-deps
 update-deps:
@@ -15,7 +32,7 @@ update-deps:
 	go mod tidy -v
 	go mod vendor
 
-.PHONY: add-dep
+.PHONY: add-deps
 add-dep:
 	go mod tidy -v
 	go mod vendor
--- cmd/baton-bamboohr/main.go
+++ cmd/baton-bamboohr/main.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 	"os"
 
+	cfg "github.com/conductorone/baton-bamboohr/pkg/config"
 	"github.com/conductorone/baton-bamboohr/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
-	"github.com/spf13/viper"
 	"go.uber.org/zap"
 )
 
@@ -24,7 +24,7 @@ func main() {
 		ctx,
 		"baton-bamboohr",
 		getConnector,
-		Configuration,
+		cfg.Configuration,
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
@@ -40,26 +40,38 @@ func main() {
 	}
 }
 
-func getConnector(ctx context.Context, v *viper.Viper) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, bhrc *cfg.Bamboohr) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
-	err := field.Validate(Configuration, v)
+
+	err := field.Validate(cfg.Config, bhrc)
 	if err != nil {
 		return nil, err
 	}
 
+	companyDomain := bhrc.CompanyDomain
+	if companyDomain == "" {
+		return nil, fmt.Errorf("company domain field is required")
+	}
+
+	apiKey := bhrc.ApiKey
+	if apiKey == "" {
+		return nil, fmt.Errorf("api key field is required")
+	}
+
 	cb, err := connector.New(
 		ctx,
-		v.GetString(CompanyDomainField.FieldName),
-		v.GetString(ApiKeyField.FieldName),
+		companyDomain,
+		apiKey,
 	)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
-	connector, err := connectorbuilder.NewConnector(ctx, cb)
+
+	conn, err := connectorbuilder.NewConnector(ctx, cb)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
-	return connector, nil
+	return conn, nil
 }
