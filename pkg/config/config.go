d53f961f6d5a1d92b8db18b4daaef48c3dbceb9a
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 15:58:03 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -22,6 +22,7 @@ var (
 		"base-url",
 		field.WithDescription("Override the BambooHR API URL (for testing)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 
 	ConfigurationFields = []field.SchemaField{
